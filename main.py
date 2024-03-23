eb1386f9adf49745dc53d3241a339b39679893a0
Hexvel <dimaabdukarimov1234@gmail.com>
2024-03-23 22:52:19 +0300

    added new methods for [DateTimeUtils]

--- main.py
+++ main.py
@@ -1,36 +1,29 @@
 import datetime
 
-from commands import DateTimeUtils
+from utils import DateTimeUtils
 
 if __name__ == "__main__":
-    # Get current date and time
     current_datetime = DateTimeUtils.get_current_datetime()
-    print("Current Date and Time:", current_datetime)
-
-    # Create a specific date and time
     specific_datetime = DateTimeUtils.get_specific_datetime(2022, 10, 1, 12, 30)
-    print("Specific Date and Time:", specific_datetime)
-
-    # Format the date and time
     formatted_datetime = DateTimeUtils.format_datetime(
         specific_datetime, "%Y-%m-%d %H:%M:%S"
     )
-    print("Formatted Date and Time:", formatted_datetime)
-
-    # Calculate the difference between two dates
     datetime1 = DateTimeUtils.get_specific_datetime(2022, 1, 1)
     datetime2 = DateTimeUtils.get_specific_datetime(2022, 1, 15)
     time_difference = DateTimeUtils.calculate_time_difference(datetime2, datetime1)
-    print("Time Difference:", time_difference)
-
-    # Add time to a date
     new_datetime = DateTimeUtils.add_time_to_datetime(
         datetime1, datetime.timedelta(days=7)
     )
-    print("New Date and Time (After adding 7 days):", new_datetime)
-
-    # Subtract time from a date
     new_datetime = DateTimeUtils.subtract_time_from_datetime(
         datetime2, datetime.timedelta(weeks=2)
     )
-    print("New Date and Time (After subtracting 2 weeks):", new_datetime)
+    year = DateTimeUtils.get_year(specific_datetime)
+    month = DateTimeUtils.get_month(specific_datetime)
+    day = DateTimeUtils.get_day(specific_datetime)
+    hour = DateTimeUtils.get_hour(specific_datetime)
+    minute = DateTimeUtils.get_minute(specific_datetime)
+    second = DateTimeUtils.get_second(specific_datetime)
+    weekday = DateTimeUtils.get_weekday(specific_datetime)
+    is_leap = DateTimeUtils.is_leap_year(2022)
+
+    print(year, month, day, hour, minute, second, weekday, is_leap)
--- utils/date.py
+++ utils/date.py
@@ -1,21 +1,39 @@
 import datetime
+from dataclasses import dataclass
+
+
+@dataclass
+class DateTime:
+    year: int
+    month: int
+    day: int
+    hour: int
+    minute: int
+    second: int
 
 
 class DateTimeUtils:
     @staticmethod
-    def get_current_datetime() -> datetime.datetime:
+    def get_current_datetime() -> DateTime:
         """
         Function to get the current date and time.
 
         :return: current date and time
         """
         current_datetime = datetime.datetime.now()
-        return current_datetime
+        return DateTime(
+            year=current_datetime.year,
+            month=current_datetime.month,
+            day=current_datetime.day,
+            hour=current_datetime.hour,
+            minute=current_datetime.minute,
+            second=current_datetime.second,
+        )
 
     @staticmethod
     def get_specific_datetime(
         year: int, month: int, day: int, hour: int = 0, minute: int = 0, second: int = 0
-    ) -> datetime.datetime:
+    ) -> DateTime:
         """
         Function to work with specific dates and time intervals.
 
@@ -27,11 +45,12 @@ class DateTimeUtils:
         :param second: second (default 0)
         :return: specific date and time
         """
-        specific_datetime = datetime.datetime(year, month, day, hour, minute, second)
-        return specific_datetime
+        return DateTime(
+            year=year, month=month, day=day, hour=hour, minute=minute, second=second
+        )
 
     @staticmethod
-    def format_datetime(datetime_obj: datetime.datetime, format_str: str) -> str:
+    def format_datetime(datetime_obj: DateTime, format_str: str) -> str:
         """
         Function to work with different date and time formats.
 
@@ -39,12 +58,20 @@ class DateTimeUtils:
         :param format_str: format string
         :return: formatted date and time string
         """
-        formatted_datetime = datetime_obj.strftime(format_str)
-        return formatted_datetime
+        formatted_datetime = datetime.datetime(
+            datetime_obj.year,
+            datetime_obj.month,
+            datetime_obj.day,
+            datetime_obj.hour,
+            datetime_obj.minute,
+            datetime_obj.second,
+        )
+        formatted_datetime_str = formatted_datetime.strftime(format_str)
+        return formatted_datetime_str
 
     @staticmethod
     def calculate_time_difference(
-        datetime_obj1: datetime.datetime, datetime_obj2: datetime.datetime
+        datetime_obj1: DateTime, datetime_obj2: DateTime
     ) -> datetime.timedelta:
         """
         Function to calculate the difference between dates.
@@ -53,13 +80,29 @@ class DateTimeUtils:
         :param datetime_obj2: second date and time object
         :return: time difference
         """
-        time_difference = datetime_obj1 - datetime_obj2
+        datetime1 = datetime.datetime(
+            datetime_obj1.year,
+            datetime_obj1.month,
+            datetime_obj1.day,
+            datetime_obj1.hour,
+            datetime_obj1.minute,
+            datetime_obj1.second,
+        )
+        datetime2 = datetime.datetime(
+            datetime_obj2.year,
+            datetime_obj2.month,
+            datetime_obj2.day,
+            datetime_obj2.hour,
+            datetime_obj2.minute,
+            datetime_obj2.second,
+        )
+        time_difference = datetime1 - datetime2
         return time_difference
 
     @staticmethod
     def add_time_to_datetime(
-        datetime_obj: datetime.datetime, time_delta: datetime.timedelta
-    ) -> datetime.datetime:
+        datetime_obj: DateTime, time_delta: datetime.timedelta
+    ) -> DateTime:
         """
         Function to add time to a date.
 
@@ -67,13 +110,28 @@ class DateTimeUtils:
         :param time_delta: time interval to add
         :return: new date and time object
         """
-        new_datetime = datetime_obj + time_delta
-        return new_datetime
+        datetime1 = datetime.datetime(
+            datetime_obj.year,
+            datetime_obj.month,
+            datetime_obj.day,
+            datetime_obj.hour,
+            datetime_obj.minute,
+            datetime_obj.second,
+        )
+        new_datetime = datetime1 + time_delta
+        return DateTime(
+            year=new_datetime.year,
+            month=new_datetime.month,
+            day=new_datetime.day,
+            hour=new_datetime.hour,
+            minute=new_datetime.minute,
+            second=new_datetime.second,
+        )
 
     @staticmethod
     def subtract_time_from_datetime(
-        datetime_obj: datetime.datetime, time_delta: datetime.timedelta
-    ) -> datetime.datetime:
+        datetime_obj: DateTime, time_delta: datetime.timedelta
+    ) -> DateTime:
         """
         Function to subtract time from a date.
 
@@ -81,5 +139,108 @@ class DateTimeUtils:
         :param time_delta: time interval to subtract
         :return: new date and time object
         """
-        new_datetime = datetime_obj - time_delta
-        return new_datetime
+        datetime1 = datetime.datetime(
+            datetime_obj.year,
+            datetime_obj.month,
+            datetime_obj.day,
+            datetime_obj.hour,
+            datetime_obj.minute,
+            datetime_obj.second,
+        )
+        new_datetime = datetime1 - time_delta
+        return DateTime(
+            year=new_datetime.year,
+            month=new_datetime.month,
+            day=new_datetime.day,
+            hour=new_datetime.hour,
+            minute=new_datetime.minute,
+            second=new_datetime.second,
+        )
+
+    @staticmethod
+    def get_year(datetime_obj: DateTime) -> int:
+        """
+        Function to get the year from a date.
+
+        :param datetime_obj: date and time object
+        :return: year
+        """
+        return datetime_obj.year
+
+    @staticmethod
+    def get_month(datetime_obj: DateTime) -> int:
+        """
+        Function to get the month from a date.
+
+        :param datetime_obj: date and time object
+        :return: month
+        """
+        return datetime_obj.month
+
+    @staticmethod
+    def get_day(datetime_obj: DateTime) -> int:
+        """
+        Function to get the day from a date.
+
+        :param datetime_obj: date and time object
+        :return: day
+        """
+        return datetime_obj.day
+
+    @staticmethod
+    def get_hour(datetime_obj: DateTime) -> int:
+        """
+        Function to get the hour from a date.
+
+        :param datetime_obj: date and time object
+        :return: hour
+        """
+        return datetime_obj.hour
+
+    @staticmethod
+    def get_minute(datetime_obj: DateTime) -> int:
+        """
+        Function to get the minute from a date.
+
+        :param datetime_obj: date and time object
+        :return: minute
+        """
+        return datetime_obj.minute
+
+    @staticmethod
+    def get_second(datetime_obj: DateTime) -> int:
+        """
+        Function to get the second from a date.
+
+        :param datetime_obj: date and time object
+        :return: second
+        """
+        return datetime_obj.second
+
+    @staticmethod
+    def get_weekday(datetime_obj: DateTime) -> int:
+        """
+        Function to get the weekday from a date (Monday = 0, Sunday = 6).
+
+        :param datetime_obj: date and time object
+        :return: weekday
+        """
+        datetime1 = datetime.datetime(
+            datetime_obj.year,
+            datetime_obj.month,
+            datetime_obj.day,
+            datetime_obj.hour,
+            datetime_obj.minute,
+            datetime_obj.second,
+        )
+        return datetime1.weekday()
+
+    @staticmethod
+    def is_leap_year(year: int) -> bool:
+        """
+        Function to check if a year is a leap year.
+
+        :param year: year
+        :return: True if it is a leap year, False otherwise
+        """
+        return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
